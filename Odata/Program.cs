9d5f57d189ef6e37eea5d99fedb1e5349722f077
Ankit Upadhyay <ankit.u900@hotmail.com>
2025-06-15 13:33:56 +0530

    step up odata

---
+++ Odata/Program.cs
@@ -0,0 +1,32 @@
+// Program.cs
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.ModelBuilder;
+using Odata.Entities;
+using Odata;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// OData setup
+var odataBuilder = new ODataConventionModelBuilder();
+odataBuilder.EntitySet<Product>("Products");
+
+builder.Services.AddControllers().AddOData(opt =>
+    opt.AddRouteComponents("odata", odataBuilder.GetEdmModel())
+       .Filter()
+       .Select()
+       .Expand()
+       .OrderBy()
+       .Count()
+       .SetMaxTop(100));
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
